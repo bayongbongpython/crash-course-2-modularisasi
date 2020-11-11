a7456f8e27f1f9c03c82b82f3f8eb1297f930f47
WORK <andi.hidayat.mail@gmail.com>
2020-11-12 00:31:43 +0700

    Menhitung dimensi segitiga

---
+++ 1. Menhitung dimensi segitiga.py
@@ -0,0 +1,9 @@
+"""
+Program Menghitung Luas Segitiga
+luas_segitiga = alas $ tinggi /2
+"""
+
+alas = 10
+tinggi = 6
+luas_segitiga = alas * tinggi /2
+print(f'segitiga dengan alas={alas} dan tinggi={tinggi} memiliki luas {luas_segitiga}')

071672b8cfac65f06aa7c74da65d71d6e3fd1347
WORK <andi.hidayat.mail@gmail.com>
2020-11-15 12:33:28 +0700

    1

--- geometry/segitiga.py
+++ geometry/segitiga.py
@@ -1,4 +1,8 @@
+from modularisasi_tahap_1 import alas, tinggi
+
 print('\nMembuat fungsi hitung_luas_segitiga')
 def hitung_luas_segitiga(alas, tinggi):
     luas_segitiga = alas * tinggi / 2
     return luas_segitiga
+
+

7f23c92eecf69ad1a3f040915f6e30de0dda000e
WORK <andi.hidayat.mail@gmail.com>
2020-11-17 08:55:00 +0700

    mengaplikasikan fungsi import

--- use_geometry.py
+++ use_geometry.py
@@ -2,6 +2,7 @@
 # import geometri.segitiga as gs
 
 from geometri.persegipanjang import hitung_luas_persegi_panjang
+
 from geometri.segitiga import hitung_luas_segitiga
 
 print(f'hitung_luas_segitiga {hitung_luas_segitiga(10, 3)}')

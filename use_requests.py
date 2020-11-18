c31fde8e6d52fdcadda55718fb7e27fb7d58ee70
WORK <andi.hidayat.mail@gmail.com>
2020-11-18 09:04:19 +0700

    .

--- use_requests.py
+++ use_requests.py
@@ -1,2 +1,11 @@
 import requests
 
+url = 'https://detik.com'
+try:
+    requests.get(url)
+    print('Sucess!')
+except Exception as e:
+    print('There is an error', e)
+print('Program ended')
+
+

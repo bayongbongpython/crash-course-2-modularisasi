eae632cd32fd225bf043834d44aed2927f3e9a1f
WORK <andi.hidayat.mail@gmail.com>
2020-11-18 09:11:16 +0700

    .

--- use_requests.py
+++ use_requests.py
@@ -4,6 +4,7 @@ url = 'https://detik.com'
 try:
     response = requests.get(url)
     print(f'Sucess! {response}')
+    print(f'Content {response.text}')
 except Exception as e:
     print('There is an error', e)
 print('Program ended')

5613c8291186a7b703ce82336ed2b03361067d67
WORK <andi.hidayat.mail@gmail.com>
2020-11-18 09:22:36 +0700

    .

--- use_requests.py
+++ use_requests.py
@@ -3,8 +3,11 @@ import requests
 url = 'https://detik.com'
 try:
     response = requests.get(url)
-    print(f'Sucess! {response}')
-    print(f'Content {response.text}')
+    if response.status_code == 200:
+        print(f'Sucess! {response}')
+        print(f'Content {response.text}')
+    else:
+        print(f'Woops, ada kesalahan requests {response.status_code}')
 except Exception as e:
     print('There is an error', e)
 print('Program ended')
